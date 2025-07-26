f49ab23388568dc34c6a5378b7f08862f11dc6d7
unknown <franklinluiz2000@gmail.com>
2025-07-26 10:42:27 -0300

    Implementado a tela de catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,5 @@
-import './App.css'
-import ButtonInverse from './components/ButtonInverse';
-import ButtonPrimary from './components/ButtonPrimary';
-import HeaderClient from './components/HeaderClient';
-import ProductDetailsCard from './components/ProductDetailsCard';
-
+import Catalog from "./routes/Catalog"
 
 export default function App() {
-  return (
-    <>
-      
-      <main>
-        <HeaderClient />
-        <section id="product-details-section" className="dsc-container">
-          <ProductDetailsCard />
-          <div className="dsc-btn-page-container">
-            <ButtonPrimary />
-            <ButtonInverse />
-          </div>
-        </section>
-      </main>
-    </>
-  );
+  return <Catalog />
 }
-
